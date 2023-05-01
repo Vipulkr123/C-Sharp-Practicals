442ed509329e18641ecd302fffa691dcef03dc1b
Vipul1432 <vipul.v@simformsolutions.com>
2023-05-01 14:07:44 +0530

    Feat: warning for entered not number

--- Practical_1/Calculator/Calculator/Program.cs
+++ Practical_1/Calculator/Calculator/Program.cs
@@ -10,36 +10,53 @@ namespace Calculator
             {
                 string? Input_1;
                 string? Input_2;
-                do {
+                bool isNumber;
+                do
+                {
                     Console.WriteLine();
                     do
                     {
+                        Console.ForegroundColor = ConsoleColor.White;
                         Console.Write("Please Enter First number : ");
                         Input_1 = Console.ReadLine();
+                        if (!Regex.IsMatch(Input_1, @"^[+-]?\d+(\.\d+)?$"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("You have not entered a number !");
+                        }
                     }
                     while (!Regex.IsMatch(Input_1, @"^[+-]?\d+(\.\d+)?$"));
                     do
                     {
+                        Console.ForegroundColor = ConsoleColor.White;
                         Console.Write("Please Enter Second number : ");
+
                         Input_2 = Console.ReadLine();
-                    }
-                    while (!Regex.IsMatch(Input_1, @"^[+-]?\d+(\.\d+)?$"));
+                        if (!Regex.IsMatch(Input_2, @"^[+-]?\d+(\.\d+)?$"))
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("You have not entered a number !");
+                        }
+                    } while (!Regex.IsMatch(Input_2, @"^[+-]?\d+(\.\d+)?$"));
                     double Number_1 = Convert.ToDouble(Input_1);
                     double Number_2 = Convert.ToDouble(Input_2);
-                if (Number_2 != 0)
-                {
-                    Console.WriteLine($"The sum of {Number_1} and {Input_2} is {Math.Round((Number_1 + Number_2), 4)}");
-                    Console.WriteLine($"The subtraction  of {Number_1} and {Input_2} is {Math.Round((Number_1 - Number_2), 4)}");
-                    Console.WriteLine($"The multiplication of {Number_1} and {Input_2} is {Math.Round((Number_1 * Number_2), 4)}");
-                    Console.WriteLine($"The division  of {Number_1} and {Input_2} is {Math.Round((Number_1 / Number_2), 4)}");
-                }
-                else
-                {
-                    Console.WriteLine($"The {Input_1} can't Divide by 0");
-                }
+                    if (Number_2 != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"The sum of {Number_1} and {Input_2} is {Math.Round((Number_1 + Number_2), 4)}");
+                        Console.WriteLine($"The subtraction  of {Number_1} and {Input_2} is {Math.Round((Number_1 - Number_2), 4)}");
+                        Console.WriteLine($"The multiplication of {Number_1} and {Input_2} is {Math.Round((Number_1 * Number_2), 4)}");
+                        Console.WriteLine($"The division  of {Number_1} and {Input_2} is {Math.Round((Number_1 / Number_2), 4)}");
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"The {Input_1} can't Divide by 0");
+                    }
+                    Console.ForegroundColor = ConsoleColor.Blue;
                     Console.Write("Do you want to continue : y/n => ");
                 }
-            while (Console.ReadKey().KeyChar.ToString().ToLower() == "y");
+                while (Console.ReadKey().KeyChar.ToString().ToLower() == "y");
             }
         }
     }
