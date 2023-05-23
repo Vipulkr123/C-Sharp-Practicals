ea0d59c0ba2afc0bc1d0c4264c9c753fdf4a489b
Vipul1432 <vipul.v@simformsolutions.com>
2023-05-23 17:34:28 +0530

    Practical 2

---
+++ Practical_2/Practical2/CustomerAccount.cs
@@ -0,0 +1,21 @@
+﻿namespace Practical2
+{
+    internal class CustomerAccount
+    {
+        public string BankName = default!;
+        private readonly long _customerAccountNo;
+        private readonly string _customerName;
+        public CustomerAccount(long accountNo, string customerName)
+        {
+            this._customerAccountNo = accountNo;
+            this._customerName = customerName;
+        }
+        /// <summary>
+        /// It Print all the Bank details of customer account
+        /// </summary>
+        public void printInfo()
+        {
+            Console.WriteLine($"Bank : {BankName} \nCustomer Name : {_customerName} \nAccount No : {_customerAccountNo}");
+        }
+    }
+}
---
+++ Practical_2/Practical2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Practical2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\n********* Customer Bank Detail ***********\n");
+            CustomerAccount ca = new(123456,"Vipul Upadhyay");
+            ca.BankName = "SBI";
+            ca.printInfo();
+            Console.WriteLine("\n******************************************\n");
+            CustomerAccount custAcc = new(790369, "Bhavin Karelia");
+            custAcc.BankName = "Bank of India";
+            custAcc.printInfo();
+            Console.WriteLine("\n******************************************\n");
+            CustomerAccount custAccount = new(907345, "Jil Patel");
+            custAccount.BankName = "Bank of Baroda";
+            custAccount.printInfo();
+        }
+
+    }
+}
