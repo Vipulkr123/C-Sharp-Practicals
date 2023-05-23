a35dab0f8892956786e261acc139504761248986
Vipul1432 <vipul.v@simformsolutions.com>
2023-05-23 17:37:44 +0530

    Add: Practical 5

---
+++ Practical_5/Practical_5/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Practical_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[5] { 1, 2, 3, 4, 5};
+
+            for(int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    Console.WriteLine(arr[i]);
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("This is finally block");
+                }
+            }
+        }
+    }
+}
