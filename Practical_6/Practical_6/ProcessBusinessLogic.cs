1cf3c1ee9a1a8c0bda3e29e28bbf261b6dcd5ab1
Vipul1432 <vipul.v@simformsolutions.com>
2023-05-23 17:39:14 +0530

    Add: Practical 6

---
+++ Practical_6/Practical_6/ProcessBusinessLogic.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Practical_6
+{
+    public delegate void EventHandler();
+    internal class ProcessBusinessLogic
+    {
+        public event EventHandler ProcessCompleted;
+        public void StartProcess()
+        {
+            Console.WriteLine("Process Started!");
+            OnProcessCompleted();
+        }
+        protected virtual void OnProcessCompleted()
+        { 
+            //protected virtual method      
+            //if ProcessCompleted is not null then call delegate
+            ProcessCompleted?.Invoke();
+        }
+    }
+}
---
+++ Practical_6/Practical_6/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practical_6
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessBusinessLogic processBusinessLogic = new ProcessBusinessLogic();
+            processBusinessLogic.ProcessCompleted += pbl_ProcessCompleted;
+            processBusinessLogic.StartProcess();
+        }
+
+        //! Event Handler
+        public static void pbl_ProcessCompleted()
+        {
+            Console.WriteLine("Method Invoked");
+        }
+    }
+}
