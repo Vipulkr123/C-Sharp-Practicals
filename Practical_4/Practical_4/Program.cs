18663475914327cf1182a56a73f912324327f649
Vipul1432 <vipul.v@simformsolutions.com>
2023-05-23 17:36:29 +0530

    Add: Practical 4

---
+++ Practical_4/Practical_4/Program.cs
@@ -0,0 +1,51 @@
+﻿using Practical_4;
+using System.Text.RegularExpressions;
+namespace Practical_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int YourChoice = 0;
+            Student student = new Student();
+            do
+            {
+                Console.Write("Please Enter Student Name : ");
+                student.Name = Console.ReadLine();
+            } while (!Regex.IsMatch(student.Name, @"^[\p{L} \.\-]+$"));
+            for (int i = 0; i < 5; i++)
+            {
+                do
+                {
+                    Console.Write($"Enter {i + 1} Subject Marks : ");
+                } while (!(decimal.TryParse(Console.ReadLine(), out student.Marks[i]) && student.Marks[i] <= 100 && student.Marks[i] >= 0));
+            }
+
+            do
+            {
+                Console.WriteLine("\nChoose 1,2,3,4");
+                Console.WriteLine("1 - Aggregate");
+                Console.WriteLine("2 - Min Mark");
+                Console.WriteLine("3 - Maximum Mark");
+                Console.WriteLine("4 - Grade");
+                do
+                {
+                    Console.Write($"Please Enter Your Choice(1-4) : ");
+                } while (!(int.TryParse(Console.ReadLine(), out YourChoice) && YourChoice <= 4 && YourChoice >= 1));
+                Student.AverageMarks = student.CalculateAverageMarks();
+                string Grade = student.CalculateGrade(Student.AverageMarks);
+                string Result = YourChoice switch
+                {
+                    (int)Options.Aggregate => $"{student.Name} : {Student.AverageMarks}",
+                    (int)Options.MinMark => $" {student.Marks.Min()}",
+                    (int)Options.MaximumMark => $" {student.Marks.Max()}",
+                    (int)Options.Grade => $" {Grade}",
+                    _ => "Invalid Number",
+                };
+                Console.WriteLine(Result);
+                Console.Write("Do you want to continue y/n : ");
+            } while (Console.ReadKey().KeyChar.ToString().ToLower() == "y");
+        }
+
+    }
+}
---
+++ Practical_4/Practical_4/Student.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Practical_4
+{
+    internal class Student
+    {
+        public string Name;
+        public decimal[] Marks =  new decimal[5];
+        public static decimal AverageMarks;
+
+        public decimal CalculateAverageMarks()
+        {
+            for(int i = 0; i < 5; i++) 
+            { 
+                AverageMarks += Marks[i];
+            }
+            return AverageMarks / 5;
+        }
+        public string CalculateGrade(decimal marks) =>
+            marks switch
+            {
+                > 90 => "A",
+                > 80 => "B",
+                > 70 => "C",
+                < 70 => "D",
+                _ => "Invalid data",
+            };
+        
+    }
+    enum Options
+    {
+        Aggregate = 1,
+        MinMark = 2,
+        MaximumMark = 3,
+        Grade = 4
+    }
+}
